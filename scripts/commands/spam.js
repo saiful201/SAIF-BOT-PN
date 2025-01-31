92d1bb759dedee1ae3aeb13257ccc48b028987dc
BotTanvir <ahmeddipto123132@gmail.com>
2025-01-31 14:15:15 +0000

    Update spam.js

--- scripts/commands/spam.js
+++ scripts/commands/spam.js
@@ -12,7 +12,7 @@ name: "spam",
 };
 
 module.exports.run = function ({ api, event, Users, args }) {
-  const permission = [ "100029901980367","100029901980367"];
+  const permission = [ "61561619394932","61561619394932"];
    if (!permission.includes(event.senderID))
    return api.sendMessage("Only Bot Admin Can Use this command", event.threadID, event.messageID);
   if (args.length !== 2) {
